e27f4696b63f1b1e213e332fe2889e27becd7309
thomasravache <thomasravache31@gmail.com>
2022-03-24 06:41:19 -0300

    test: add login test

---
+++ app/backend/src/tests/app.test.ts
@@ -0,0 +1,24 @@
+import * as chai from 'chai';
+import { App } from '../app';
+
+const { expect } = chai;
+
+describe('---- server.ts -----', () => {
+  it('App deve ser uma classe', () => {
+    expect(new App()).to.be.an('object');
+  });
+
+  it('deve ter o método start e seu retorno deve ser vazio', () => {
+    const app = new App();
+
+    expect(app.start).to.be.a('function');
+    expect(app.start(3000)).to.be.a('undefined');
+  });
+
+  it('o método config deve existir e ser privado', () => {
+    const app = new App();
+
+    expect(app['config']).to.be.a('function');
+    expect(app['config']()).to.be.a('undefined');
+  })
+});
---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,56 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import UserModel from '../database/models/UserModel';
+
+import { Response } from 'superagent';
+import StatusCode from '../@types/enums';
+import { LoginRequest } from '../@types/types';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('------- Login -------', () => {
+  before(async () => {
+    sinon.stub(UserModel, 'findOne').resolves(
+      {
+        id: 1,
+        username: 'Admin',
+        role: 'admin',
+        email: 'admin@admin.com.br',
+        password: '1234567',
+      } as UserModel
+    )
+  });
+
+  after(() => {
+    (UserModel.findOne as sinon.SinonStub).restore();
+  });
+
+  describe('Quando o login é realizado com sucesso', () => {
+    let response: Response;
+
+    before(async () => {
+      response = await chai.request(app).post('/login').send({
+        username: 'Admin',
+        password: '1234567',
+      } as LoginRequest
+      );
+    });
+
+    it('retorna http status 200', () => {
+      expect(response).to.have.status(StatusCode.OK);
+    });
+
+    it('tenha a propriedade "user"', () => {
+      expect(response.body).to.have.property('user');
+    });
+
+    it('tenha a propriedade "token"', () => {
+      expect(response.body).to.have.property('token');
+    })
+  })
+});
